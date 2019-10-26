547528ca263ad50a613517e0bc6012e35c128198
procek69 <s13833@pjwstk.edu.pl>
2019-10-26 12:55:42 +0200

    First base commit

---
+++ rm_scanner/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  rm_scanner
+//
+//  Created by stud on 26/10/2019.
+//  Copyright © 2019 ol. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ rm_scanner/FirstViewController.m
@@ -0,0 +1,23 @@
+//
+//  FirstViewController.m
+//  rm_scanner
+//
+//  Created by stud on 26/10/2019.
+//  Copyright © 2019 ol. All rights reserved.
+//
+
+#import "FirstViewController.h"
+
+@interface FirstViewController ()
+
+@end
+
+@implementation FirstViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
